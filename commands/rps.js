61c76b680e41651f82eb761f9c80ac7259588df7
Bradley Cornford <bradley.cornford@ask4.com>
2020-10-14 10:27:52 +0100

    Added command rps.

---
+++ commands/rps.js
@@ -0,0 +1,55 @@
+const {validateMessageFromInput} = require("../includes/input");
+module.exports = {
+    name: 'rps',
+    description: 'Play rock paper scissors',
+    parameters: ['[', 'rock', '|', 'paper', '|', 'scissors', ']'],
+    hidden: false,
+    run: async (client, message, args) => {
+        let userChoice = args.join(' ');
+
+        if (
+            !validateMessageFromInput(args, message) ||
+            (
+                !userChoice.includes('rock') &&
+                !userChoice.includes('paper') &&
+                !userChoice.includes('scissors')
+            )
+        ) {
+            return message;
+        }
+
+        switch (userChoice) {
+            case 'rock':
+                userChoice = 0;
+                break;
+            case 'paper':
+                userChoice = 1;
+                break;
+            case 'scissors':
+                userChoice = 2;
+                break;
+        }
+
+        let choices = ['✊', '✋', '✌️️'];
+        let botChoice = Math.floor(Math.random() * choices.length);
+        let winner;
+
+        if (
+            (botChoice === 0 && userChoice === 2) ||
+            (botChoice === 1 && userChoice === 0) ||
+            (botChoice === 2 && userChoice === 1)
+        ) {
+            winner = 'Winner @gumbot!';
+        } else if (
+            (userChoice === 0 && botChoice === 2) ||
+            (userChoice === 1 && botChoice === 0) ||
+            (userChoice === 2 && botChoice === 1)
+        ) {
+            winner = `Winner @${message.author.username}!`;
+        } else if (botChoice === userChoice) {
+            winner = 'It\'s a draw!';
+        }
+
+        return message.channel.send(`🕹️ **Rock paper scissors:** I choose ${choices[botChoice]} . ${winner}`);
+    },
+};
