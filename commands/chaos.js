cc9c685198319cb8d6b2571bcfe631e1fdba330d
Bradley Cornford <bradley.cornford@ask4.com>
2020-10-24 10:15:45 +0100

    Fixing time issue with chaos and creature.

--- commands/chaos.js
+++ commands/chaos.js
@@ -7,9 +7,9 @@ module.exports = {
     parameters: [],
     hidden: false,
     run: async (client, message, args) => {
-        let initialChaosEndTime = moment('11-10-2020 01:00:00', 'DD-MM-YYYY hh:mm:ss');
-        let now = moment();
-        let chaosInterval = ms('14d 4h');
+        let initialChaosEndTime = moment('11-10-2020 01:00:00', 'DD-MM-YYYY hh:mm:ss').tz('Europe/London');
+        let now = moment().tz('Europe/London');
+        let chaosInterval = ms('14d 2h');
         let chaosPeriod = ms('12h');
         let chaosCount = (Math.floor(now.diff(initialChaosEndTime) / chaosInterval));
         let nextChaosTime = initialChaosEndTime.add((chaosInterval * (chaosCount + 1)) + (chaosPeriod * chaosCount));
